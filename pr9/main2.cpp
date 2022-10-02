fbd9acb4b8202d2ddde03ead8b638a52e6ac438a
aqude <aquadefin@gmail.com>
2022-10-02 16:26:58 +0300

    fix

--- pr9/main2.cpp
+++ pr9/main2.cpp
@@ -7,21 +7,43 @@ int random(int min, int max) {
 }
 
 int main() {
-    int *parr = nullptr;
     const int size = 10;
     int arr[size][size];
-    // int &link_size = size;
 
+    // заполнить матрицу случайными числами
     for (int i = 0; i < size; i++)
-    {
         for (int j = 0; j < size; j++)
-        {
-            arr[i][j] = random(-50, 50);
-        }
-        
+            arr[i][j] = random(0, 20);
+
+    // вывести матрицу на экран
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++)
+            cout << arr[i][j] << " ";
+        cout << endl;
+    }
+    cout << "=====================" << endl;
+
+    // упорядочить элементы матрицы по убыванию в строках которые не содержат нулевых элементов
+    for (int i = 0; i < size; i++) {
+        bool isZero = false;
+        for (int j = 0; j < size; j++)
+            if (arr[i][j] == 0) {
+                isZero = true;
+                break;
+            }
+        if (isZero)
+            continue;
+        for (int j = 0; j < size - 1; j++)
+            for (int k = j + 1; k < size; k++)
+                if (arr[i][j] < arr[i][k])
+                    swap(arr[i][j], arr[i][k]);
+    }
+    // вывести матрицу на экран
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++)
+            cout << arr[i][j] << " ";
+        cout << endl;
     }
 }
 
-// Задача 2. Дана целочисленная квадратная матрица. Упорядочить по
-// убыванию элементы в тех строках, которые не содержат ни одного нулевого
-// элемента.
+
